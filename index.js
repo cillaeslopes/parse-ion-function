bee6a43b5dc7fe25b631fa542ac2a479b36658ac
Pri Lopes <cillaeslopes@gmail.com>
2022-06-24 13:46:52 -0300

    fix ion loading

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const ion = require("ion-js");
 exports.handler = async (event, context) => {
   /* Process the list of records and transform them */
   const output = event.records.map((record) => {
-    const buffer = Buffer.from(record.data, "base64").toString();
+    const buffer = Buffer.from(record.data, "base64");
     const ionData = ion.load(buffer);
     const data = JSON.parse(JSON.stringify(ionData));
-
+    
     const isRevisionDetailsBlock = data.recordType === "REVISION_DETAILS";
     if (isRevisionDetailsBlock) {
       const parsedData = Buffer.from(JSON.stringify(data)).toString("base64");
@@ -16,6 +16,8 @@ exports.handler = async (event, context) => {
         result: 'Ok',
         data: parsedData,
       };
+    } else {
+      console.log(`Ignoring blocks of type ${data.recordType}`);
     }
   }).filter((record) => !!record);
   console.log(`Processing completed.  Successful records ${output.length}.`);
